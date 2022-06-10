20cc9cfd8ba6227060f353d0af3586eb499937d3
Brooke Love <savienloveb@gmail.com>
2022-06-10 09:07:57 -0700

    renbaming and structuring files

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Matchup from './pages/Matchup';
+import Vote from './pages/Vote';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex-column justify-center align-center min-100-vh bg-primary">
+        <Routes>
+          <Route 
+            path="/" 
+            element={<Home />} 
+          />
+          <Route 
+            path="/matchup" 
+            element={<Matchup />} 
+          />
+          <Route 
+            path="/matchup/:id" 
+            element={<Vote />} 
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
